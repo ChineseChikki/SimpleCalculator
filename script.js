8d5bfde8fe5150d7a732493c4e9c387bd3d10d26
ChineseChikki <chinelsy@gmail.com>
2022-09-16 09:12:01 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+// This function clear all the values
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
+
+// This function display values
+function display(value) {
+  document.getElementById("result").value += value;
+}
+
+// This function evaluates the expression and returns result
+function calculate() {
+  const input = document.getElementById("result").value;
+  const answer = eval(input);
+  document.getElementById("result").value = answer;
+}
